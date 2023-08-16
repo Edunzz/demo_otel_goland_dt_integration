b9563a730d3caf6f9d3259c7cb7519592c71d327
JoseRomero616 <jo.seeduardo25397stk@gmail.com>
2023-08-16 10:57:23 -0500

    Update main.go

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"github.com/gin-gonic/gin"
 	"github.com/go-sql-driver/mysql"
-	_ "./docs" 
+	_ "./docs.go" 
 	"github.com/swaggo/gin-swagger"
 	"github.com/swaggo/gin-swagger/swaggerFiles"
 	"go.opentelemetry.io/otel"
